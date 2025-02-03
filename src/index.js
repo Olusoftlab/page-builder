196eb9575ea52d22d358c415b3840ed77a90edb5
olumild <olumild998@gmail.com>
2025-02-03 19:33:35 +0100

    adjust

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // import App from "./App"
 import ReactDom from "react-dom/client"
-import Ready from "./Ready"
+// import Ready from "./Ready"
+import App from "./App"
 import "./index.css"
 
 
 const root = ReactDom.createRoot(document.getElementById("root"))
 
-root.render(<Ready />)
+root.render(<App />)
 
